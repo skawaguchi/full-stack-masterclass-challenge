aec189c177cb45e59a20aeb3a63ed063d32a4d07
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2018-01-06 18:10:22 -0500

    chore: eslint fix

--- src/client/src/components/CloseLink.spec.js
+++ src/client/src/components/CloseLink.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Link } from 'react-router-dom';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/fontawesome-free-solid'
+import { faTimes } from '@fortawesome/fontawesome-free-solid';
 
 import CloseLink from './CloseLink';
 
