d256e260f0de601d7386af075874c3bf51fda7a6
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2018-01-07 17:37:41 -0500

    feat: add content for product details

--- src/client/src/views/product-details/ProductDetailsContent.js
+++ src/client/src/views/product-details/ProductDetailsContent.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { inject, observer } from 'mobx-react';
+import { FormattedNumber } from 'react-intl';
 
 import CloseLink from '../../components/CloseLink';
 import ProductImage from '../../components/ProductImage';
 
 import './ProductDetailsContent.css';
 
+function getOptionalContent(config, product) {
+    return product[config.attr] ?
+        <div className={ config.className }>
+            <dt>{ config.label }</dt>
+            <dd>{ product[config.attr] }</dd>
+        </div> :
+        null;
+}
+
 @inject('productListStore')
 @observer
 class ProductDetailsContent extends Component {
     render() {
         const product = this.props.productListStore.getDisplayedProductDetails(this.props.id);
+        const tastingNoteConfig = {
+            attr: 'tastingNote',
+            className: 'tasting-note',
+            label: 'Tasting Note'
+        };
+        const varietalConfig = {
+            attr: 'varietal',
+            className: 'varietal',
+            label: 'Varietal'
+        };
+        const styleConfig = {
+            attr: 'style',
+            className: 'style',
+            label: 'Style'
+        };
 
         return (
             <section className="product-details">
@@ -20,6 +45,28 @@ class ProductDetailsContent extends Component {
                     altText={ product.name }
                     imagePath={ product.imagePath }
                 />
+                <h1>{ product.name }</h1>
+                <dl className="product-details-content">
+                    <div className="product-id">
+                        <dt>{ 'Product ID' }</dt>
+                        <dd>{ product.id }</dd>
+                    </div>
+
+                    <div className="price">
+                        <dt>{'Price'}</dt>
+                        <dd>
+                            <FormattedNumber
+                                currency='USD'
+                                style='currency'
+                                value={ product.price }
+                            />
+                        </dd>
+                    </div>
+
+                    { getOptionalContent(tastingNoteConfig, product) }
+                    { getOptionalContent(varietalConfig, product) }
+                    { getOptionalContent(styleConfig, product) }
+                </dl>
             </section>
         );
     }
--- src/client/src/views/product-details/ProductDetailsContent.spec.js
+++ src/client/src/views/product-details/ProductDetailsContent.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { FormattedNumber } from 'react-intl';
 
 import ProductDetailsContent from './ProductDetailsContent';
 
@@ -30,17 +31,21 @@ describe('<ProductDetailsContent/>', () => {
         ).dive();
     }
 
+    function setupMockStore(overrides) {
+        productMock = getAdaptedProduct(overrides);
+
+        store.productList = [
+            productMock
+        ];
+    }
+
     beforeEach(() => {
         store = Stores.productListStore;
     });
 
-    describe.only('Given products are loaded', () => {
+    describe('Given products are loaded', () => {
         beforeEach(() => {
-            productMock = getAdaptedProduct();
-
-            store.productList = [
-                productMock
-            ];
+            setupMockStore();
 
             renderWrappedComponent();
         });
@@ -59,6 +64,132 @@ describe('<ProductDetailsContent/>', () => {
 
             expect(image.props().altText).toEqual(productMock.name);
             expect(image.props().imagePath).toEqual(productMock.imagePath);
+        });
+
+        it('should display the product name prominently', () => {
+            const header = component.find('h1');
+
+            expect(header.text()).toEqual(productMock.name);
+        });
+    });
+
+
+    describe('Product details', () => {
+        describe('Always displayed', () => {
+            beforeEach(() => {
+                setupMockStore();
+
+                renderWrappedComponent();
+            });
+
+            it('should contain the product identifier', () => {
+                const container = component.find('div.product-id');
+                const label = container.find('dt');
+                const value = container.find('dd');
+
+                expect(label.text()).toEqual('Product ID');
+                expect(value.text()).toEqual(productMock.id);
+            });
+
+            it('should contain the product price demarcated as USD but really in CAD due to the underlying Intl API formatting', () => {
+                const container = component.find('div.price');
+                const label = container.find('dt');
+                const value = container.find(FormattedNumber);
+
+                expect(label.text()).toEqual('Price');
+                expect(value.props().currency).toEqual('USD');
+                expect(value.props().style).toEqual('currency');
+                expect(value.props().value).toEqual(productMock.price);
+            });
+        });
+
+        describe('Displayed dependent on data', () => {
+            describe('Given there is tasting note data', () => {
+                it('should have a tasting note', () => {
+                    setupMockStore();
+
+                    renderWrappedComponent();
+
+                    const container = component.find('div.tasting-note');
+                    const label = container.find('dt');
+                    const value = container.find('dd');
+
+                    expect(label.text()).toEqual('Tasting Note');
+                    expect(value.text()).toEqual(productMock.tastingNote);
+                });
+            });
+
+            describe('Given there is no tasting note data', () => {
+                it('should not display a tasting note', () => {
+                    setupMockStore({
+                        tastingNote: null
+                    });
+
+                    renderWrappedComponent();
+
+                    const container = component.find('div.tasting-note');
+
+                    expect(container).toHaveLength(0);
+                });
+            });
+
+            describe('Given there is varietal data', () => {
+                it('should have a varietal note', () => {
+                    setupMockStore();
+
+                    renderWrappedComponent();
+
+                    const container = component.find('div.varietal');
+                    const label = container.find('dt');
+                    const value = container.find('dd');
+
+                    expect(label.text()).toEqual('Varietal');
+                    expect(value.text()).toEqual(productMock.varietal);
+                });
+            });
+
+            describe('Given there is no varietal data', () => {
+                it('should not display a varietal note', () => {
+                    setupMockStore({
+                        varietal: null
+                    });
+
+                    renderWrappedComponent();
+
+                    const container = component.find('div.varietal');
+
+                    expect(container).toHaveLength(0);
+                });
+            });
+
+            describe('Given there is style data', () => {
+                it('should have a style note', () => {
+                    setupMockStore();
+
+                    renderWrappedComponent();
+
+                    const container = component.find('div.style');
+                    const label = container.find('dt');
+                    const value = container.find('dd');
+
+                    expect(label.text()).toEqual('Style');
+                    expect(value.text()).toEqual(productMock.style);
+                });
+            });
+
+            describe('Given there is no style data', () => {
+                it('should not display a style note', () => {
+                    setupMockStore({
+                        style: null
+                    });
+
+                    renderWrappedComponent();
+
+                    const container = component.find('div.style');
+
+                    expect(container).toHaveLength(0);
+                });
+            });
         });
     });
 });
