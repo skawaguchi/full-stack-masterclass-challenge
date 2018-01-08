0c9f3bc1956c319196925047a38d17edd516ed22
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2018-01-08 05:26:37 -0500

    chore: display the easy data
    - still need to work on the more challenging data (hours) and add the directions button

--- src/client/src/views/beer-finder/StoreListTableRowItem.js
+++ src/client/src/views/beer-finder/StoreListTableRowItem.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/fontawesome-free-solid';
+import { FormattedNumber } from 'react-intl';
 
-function StoreListTableRowItem() {
+function getAttribute(value) {
+    return value !== null ? <div>{ value }</div> : null;
+}
+
+function StoreListTableRowItem(props) {
     return (
         <tr>
+            <td className="distance">
+                <FormattedNumber
+                    maximumFractionDigits={1}
+                    value={ props.item.distance }
+                />
+                <span className="unit-label">{'km'}</span>
+            </td>
+            <td className="quantity">
+                <FormattedNumber
+                    value={ props.item.quantity }
+                />
+            </td>
+            <td className="store-info">
+                { getAttribute(props.item.addressLine1) }
+                { getAttribute(props.item.addressLine2) }
+                { getAttribute(props.item.city) }
+                <div>
+                    <span className="store-label">{'Store #'}</span>
+                    <span className="store-value">{ props.item.id }</span>
+                </div>
+            </td>
+            <td className="hours"></td>
+            <td className="telephone">
+                { getAttribute(props.item.telephone) }
+            </td>
         </tr>
     );
 }
 
 StoreListTableRowItem.propTypes = {
     item: PropTypes.shape({
+        distance: PropTypes.number.isRequired
     })
 };
 
--- src/client/src/views/beer-finder/StoreListTableRowItem.spec.js
+++ src/client/src/views/beer-finder/StoreListTableRowItem.spec.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/fontawesome-free-solid';
+import { FormattedNumber } from 'react-intl';
 
 import StoreListTableRowItem from './StoreListTableRowItem';
 
@@ -10,7 +14,6 @@ describe('<StoreListTableRowItem/>', () => {
     let props;
 
     function renderComponent(overrides) {
-
         props = Object.freeze({
             item: getAdaptedStore(overrides)
         });
@@ -27,6 +30,57 @@ describe('<StoreListTableRowItem/>', () => {
 
         it('should have a container element', () => {
             expect(component.type()).toEqual('tr');
+        });
+
+        it('should display the distance', () => {
+            const container = component.find('td.distance');
+            const number = container.find(FormattedNumber);
+            const unit = container.find('span.unit-label');
+
+            expect(number.props().maximumFractionDigits).toEqual(1);
+            expect(number.props().value).toEqual(props.item.distance);
+            expect(unit.text()).toEqual('km');
+        });
+
+        it('should display the quantity', () => {
+            const container = component.find('td.quantity');
+            const number = container.find(FormattedNumber);
+
+            expect(number.props().value).toEqual(props.item.quantity);
+        });
+
+        it('should display the store info', () => {
+            const container = component.find('td.store-info');
+
+            expect(container.text()).toContain(props.item.addressLine1);
+            expect(container.text()).toContain(props.item.addressLine2);
+            expect(container.text()).toContain(props.item.city);
+            expect(container.text()).toContain(props.item.id);
+        });
+
+        it('should display the hours', () => {
+            const container = component.find('td.hours');
+
+            expect(container).toHaveLength(1);
+        });
+
+        it('should display the phone', () => {
+            const container = component.find('td.telephone');
+
+            expect(container.text()).toContain(props.item.telephone);
+        });
+    });
+
+
+    describe('Given there is no address line 2', () => {
+        it('should not display the line', () => {
+            renderComponent({
+                addressLine2: null
+            });
+
+            const container = component.find('td.store-info');
+
+            expect(container.text()).not.toContain(props.item.addressLine2);
         });
     });
 });
