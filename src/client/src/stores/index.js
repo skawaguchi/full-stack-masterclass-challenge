e928e5f64dcb421a56b79686d684fb81667c6a3a
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2018-01-08 00:25:53 -0500

    chore: expose the stores store to the index

--- src/client/src/stores/index.js
+++ src/client/src/stores/index.js
@@ -1,5 +1,7 @@
 import ProductListStore from './ProductListStore';
+import StoreListStore from './StoreListStore';
 
 export const Stores = {
-    productListStore: new ProductListStore()
+    productListStore: new ProductListStore(),
+    storeListStore: new StoreListStore()
 };
--- src/client/src/stores/index.spec.js
+++ src/client/src/stores/index.spec.js
@@ -1,8 +1,13 @@
 import { Stores } from './index';
 import { ProductListStore } from './ProductListStore';
+import { StoreListStore } from './StoreListStore';
 
 describe('Stores', () => {
     it('should have a product list store', () => {
         expect(Stores.productList).toEqual(ProductListStore);
+    });
+
+    it('should have an lcbo store list store', () => {
+        expect(Stores.storeList).toEqual(StoreListStore);
     });
 });
